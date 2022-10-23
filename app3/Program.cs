8d7d7552649d4359a6709ce5c6024cddcb856464
a1venest <aivenest@gmail.com>
2022-10-23 10:42:50 +0500

    app3 ver1

---
+++ app3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Написать рекурсивный метод деления
+double GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void DividingNumbers(double n1, double n2)
+{
+    double value = n1%n2;
+    double realms = n1/n2;
+    Console.WriteLine($"Результат деления {realms}");
+    Console.WriteLine($"Остаток {value}");
+}
+
+
+double m = GetNumberFromConsole("Введите первое число:");
+double n = GetNumberFromConsole("Введите второе число:");
+DividingNumbers(m,n);
